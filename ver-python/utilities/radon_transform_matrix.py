963b451d7e70bb12a79c225f4961cef7202ed4d4
fedorgoncharov <fedor.goncharov.ol@gmail.com>
2020-03-16 23:36:22 +0100

    modified radon transforms computations -- speed optimized, tested em-algorithm - ok, added poisson-noise function for matrix data - ok, added readme file for em folder

--- ver-python/utilities/radon_transform_matrix.py
+++ ver-python/utilities/radon_transform_matrix.py
@@ -12,7 +12,6 @@ Created on Fri Mar 13 21:39:18 2020
 """
 
 import numpy as np
-from scipy import sparse
 
 def radon_transform2d_classical(image, phi, shift, radius):
     """
@@ -42,14 +41,21 @@ def radon_transform2d_classical(image, phi, shift, radius):
     
     delta = 2.0*radius / npixels
     line_pixels = np.floor((line_points + radius)/delta).astype(int)
-    integral = 0.0
-    for point in line_pixels.T:
+    line_pixels = line_pixels[:, ((line_pixels[0] > -1)*(line_pixels[0] < npixels))
+                              *((line_pixels[1] > -1)*(line_pixels[1] < npixels))]
+    
+    
+    values = image[line_pixels[1, :], line_pixels[0, :]]
+    return np.sum(values) * step
+    
+    #integral = 0.0
+    #for point in line_pixels.T:
         
-        # find pixel corresponding to point
-        if ((np.min(point) > -1) and (np.max(point) < npixels)):
-            integral += image[point[1]][point[0]]
-       
-    return integral * step
+    #    # find pixel corresponding to point
+    #    if ((np.min(point) > -1) and (np.max(point) < npixels)):
+    #        integral += image[point[1]][point[0]]
+    #   
+    #return integral * step
 
 def radon_transform2d_xray(image, nproj, nshift, radius, max_proj_angle=np.pi):
     """
@@ -103,7 +109,7 @@ def radon_transform2d_xray_matrix(npixels, nproj, nshift, radius, max_proj_angle
     # create output matrix
     matrix = np.zeros((nshift*nproj, npixels**2))
     
-    for i_pixel in range(npixels**2i):
+    for i_pixel in range(npixels**2):
         i_pixel_x = i_pixel % npixels
         i_pixel_y = i_pixel // npixels
         pixel_x = -radius + delta/2 + i_pixel_x * delta
@@ -116,8 +122,10 @@ def radon_transform2d_xray_matrix(npixels, nproj, nshift, radius, max_proj_angle
         #create sinogram
         sinogram = np.zeros((nshift, nproj))
         
+        # for each pixel compute sinogram
         for i_shift in range(nshift):
             for i_phi in range(nproj):
+                
                 shift = shifts[i_shift]
                 phi = proj_angles[i_phi]
                 if (np.abs((np.dot([pixel_x, pixel_y], [np.cos(phi), np.sin(phi)])) - shift) > delta*np.sqrt(2)):
--- ver-python/utilities/sinogram_noise_generator.py
+++ ver-python/utilities/sinogram_noise_generator.py
@@ -17,7 +17,7 @@ Created on Fri Mar 13 21:39:18 2020
 import numpy as np
 
 
-def generate_noise_xray_transmission(input_filename, output_filename, avg_intensity=1e5, T=1.0, sc_intensity=1e1):
+def generate_noise_from_file_xray_transmission(input_filename, output_filename, avg_intensity=1e5, T=1.0, sc_intensity=1e1):
     
     """
     function (1) reads ray/Radon transforms - (2) takes exponentiation exp(-Ra) -
@@ -50,10 +50,34 @@ def generate_noise_xray_transmission(input_filename, output_filename, avg_intens
     fid.close()
     
     
-def generate_noise_emission(input_filename, output_filename, N_max, N_sc):
+def generate_noise_xray_transmission(sinogram, avg_intensity=1e5, T=1.0, sc_intensity=1e1):
+    
     """
-    function (1) reads weighted ray/Radon transforms - (2) finds maximal value P_wf and normalizes data P_wf / max(P_wf) -
-    (2) generates Poisson rdvs Po(N_max * P_wf / max(P_wf) + r), where N_max is given in the arguments
+    function 
+    (1) takes on input ray/Radon transforms (sinogram) 
+    (2) takes exponentiation exp(-Ra) 
+    (3) generates Poisson rdv Po(N0 * t * exp(-Ra) + r * t), where N0 is given in the arguments
+     Arguments : 
+
+          sinogram      : matrix of ray transforms on the plane of type (nshift, nphi)
+          avg_intensity : average intensity of the source (photons / sec)
+          T : irradiation time in seconds 
+          sc_intensity  : intensity of scattered photons (scattered photons / sec)
+
+    Output : 
+          matrix of type as sinogram with Poisson noise data 
+  
+    Example : 
+          noisy_sinogram = generate_nosie_xray_transmission(sinogram, avg_intensity=1e5, sc=1e1)
+    """
+    return np.random.poisson(avg_intensity * T * np.exp(-1.0 * sinogram) + sc_intensity*T)
+    
+def generate_noise_from_file_emission(input_filename, output_filename, N_max, N_sc):
+    """
+    function 
+    (1) reads weighted ray/Radon transforms 
+    (2) finds maximal value P_wf and normalizes data P_wf / max(P_wf) -
+    (3) generates Poisson rdvs Po(N_max * P_wf / max(P_wf) + r), where N_max is given in the arguments
 
     Arguments : 
     
@@ -66,7 +90,7 @@ def generate_noise_emission(input_filename, output_filename, N_max, N_sc):
         binary file containing array with values of type np.int64
 
     Example : 
-        generate_noise_emission('ray_transforms.bin', 'noise_ray_transforms.bin', 1e2, 3)
+        generate_noise_from_file_emission('ray_transforms.bin', 'noise_ray_transforms.bin', 1e2, 3)
     """
     
     # read data
@@ -81,3 +105,24 @@ def generate_noise_emission(input_filename, output_filename, N_max, N_sc):
     output_array.tofile(fid, sep="")
     fid.close()
 
+def generate_noise_emission(sinogram, N_max, N_sc):
+    
+    """
+    function 
+    (1) takes weighted ray/Radon transforms as input matrix  
+    (2) finds maximal value P_wf and normalizes data P_wf / max(P_wf) 
+    (3) generates Poisson rdvs Po(N_max * P_wf / max(P_wf) + r), where N_max is given in the arguments
+
+    Arguments : 
+    
+        sinogram : matrix of type (nshift, nphi) with Radon transfroms on the plane
+        N_max : average maximal number of photons per ray
+        N_sc : average number of scattered photons
+
+    Output : 
+        matrix of the same type as sinogram with Radon transforms with Poisson noise
+
+    Example : 
+       noisy_sinogram=generate_noise_emission(sinogram, 1e2, 3)
+    """
+    return np.random.poisson(N_max*sinogram / np.max(sinogram[:]) + N_sc)
