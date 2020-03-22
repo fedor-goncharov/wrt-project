185f4556505f47249b680f448e25b79cbcc3d0bf
fedorgoncharov <fedor.goncharov.ol@gmail.com>
2020-03-22 22:20:52 +0100

    template for gibbs sampler

---
+++ ver-python/gibbs_sampler_emission.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 20:30:56 2020
+
+@author: fedor.goncharov.ol@gmail.com
+"""
+
+
+def gibbs_sampler_emission_gamma():
+    """
+    Gibbs sampler for emission tomography with 
+    separable Gamma-prior (i.e., without spatial regularizaiton). 
+    
+    Input
+    -------
+    
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    return None 
